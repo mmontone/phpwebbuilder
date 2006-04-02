6adb9e1a95454492a4800f089b2bc85f218e452b
alexsiri7 <alexsiri7@gmail.com>
2006-04-02 14:10:46 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@14 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- OldViews/Action/HtmlEditHtmlAreaView.class.php
+++ OldViews/Action/HtmlEditHtmlAreaView.class.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once dirname(__FILE__) . '/HtmlEditFieldView.class.php';
-require_once pwbdir . '/extra/FCKEditor/FCKEditor.php';
+require_once pwbdir . '/extra/FCKeditor/fckeditor.php';
 
 
 
