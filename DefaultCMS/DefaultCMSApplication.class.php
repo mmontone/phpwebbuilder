5be522e0862ed45e5ff2042a73d3297c8531bb05
alexsiri7 <alexsiri7@gmail.com>
2006-04-29 17:08:23 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@93 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/DefaultCMSApplication.class.php
+++ DefaultCMS/DefaultCMSApplication.class.php
@@ -1,15 +1,17 @@
 <?php
 
 class DefaultCMSApplication extends Application{
+	var $templateName = 'BFSalud';
+	//var $templateName = 'Default';
  	function &set_root_component() {
  		$cms =& new DefaultCMS;
  		return $cms;
  	}
  	function loadTemplates (){
- 		$this->viewCreator->loadTemplatesDir(pwbdir."/DefaultCMS/Templates");
+ 		$this->viewCreator->loadTemplatesDir(pwbdir . "/DefaultCMS/Templates/".$this->templateName);
  	}
  	function &setCss(){
- 		return array(site_url."/admin/DefaultCMS/standard.css");
+ 		return array(site_url."/admin/DefaultCMS/Templates/".$this->templateName.'/'.$this->templateName.'.css');
  	}
 }
 
