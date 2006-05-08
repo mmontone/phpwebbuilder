8645fa572a6cca541a055bec716d7cb2f7ac85fb
alexsiri7 <alexsiri7@gmail.com>
2006-05-08 15:14:12 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@162 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/Login.class.php
+++ DefaultCMS/Login.class.php
@@ -9,8 +9,8 @@ class Login extends Component
 	var $password;
 
 	function initialize(){
-			$this->username =& new ValueHolder($s = '');
-			$this->password =& new ValueHolder($s = '');
+			$this->username =& new ValueHolder($s1 = '');
+			$this->password =& new ValueHolder($s2 = '');
 			$this->add_component(new Input($this->username), 'comp_username');
 			$this->add_component(new Password($this->password), 'comp_password');
 			$this->add_component(new ActionLink($this, 'login_do', 'Login',$params=null), 'login');
@@ -19,13 +19,15 @@ class Login extends Component
 	}
 
 	function login_do(){
-		$success =& User::login($this->username->getValue(), $this->password->getValue());
+		$u = $this->username->getValue();
+		$p = $this->password->getValue();
+		$success =& User::login($u, $p);
 		if ($success){
 			$this->triggerEvent('menuChanged', $success);
 			$this->state->setValue($v = 'success');
 		} else {
 			$this->state->setValue($v = 'failed');
-			$this->password->setValue($p = '');
+			$this->password->setValue($p2 = '');
 		}
 	}
 }
