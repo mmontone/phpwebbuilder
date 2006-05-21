08014a8cb005df4f11b035d6e97ed5b7ccf8c7c1
alexsiri7 <alexsiri7@gmail.com>
2006-05-21 13:46:50 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@220 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/DefaultCMS.php
+++ DefaultCMS/DefaultCMS.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'DBController.class.php';
-require_once 'RolesController.class.php';
+require_once 'Admin/DBController.class.php';
+require_once 'Admin/RolesController.class.php';
 require_once 'DefaultCMS.class.php';
 require_once 'DefaultCMSApplication.class.php';
 require_once 'Menu.class.php';
-require_once 'Login.class.php';
-require_once 'Logout.class.php';
+require_once 'Users/Login.class.php';
+require_once 'Users/Logout.class.php';
 includefile($dir = pwbdir.'/DefaultCMS/CMS');
 
 ?>
