03ab0dabd00ad0ef6593c23f48e7212fe3916a1a
alexsiri7 <alexsiri7@gmail.com>
2006-05-22 00:38:04 +0000

    - Set Style for TextArea in DefaultCMS - Changed Logout to be a Component
    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@231 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/Users/Logout.class.php
+++ DefaultCMS/Users/Logout.class.php
@@ -1,9 +1,10 @@
 <?
 
-require_once pwbdir.'/Controllers/Controller.class.php';
-
 class Logout extends Component{
 	function initialize(){
+		$this->addComponent(new ActionLink($this, 'logout_do','Logout',$n=null));
+	}
+	function logout_do (){
 		User::login('guest','guest');
 		$this->triggerEvent('menuChanged', $n=null);
 	}
