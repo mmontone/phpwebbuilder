62958d67d8ea4be74a1de7a6957d03cd301440ad
mmontone <marianomontone@gmail.com>
2006-05-25 19:41:10 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@270 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ newcontroller/components/BaseComponents/CompositeFormComponent.class.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'FormComponent.class.php';
+
+class CompositeFormComponent extends FormComponent {
+
+    function CompositeFormComponent() {
+    	parent::FormComponent($vh = null);
+    }
+}
+?>
