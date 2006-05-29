919b510fc7c42e079f93f75471ae4e76a0975461
alexsiri7 <alexsiri7@gmail.com>
2006-05-29 11:39:53 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@305 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- basiclib.php
+++ basiclib.php
@@ -163,7 +163,8 @@ function toHTML($s) {
    return mb_convert_encoding($s,"HTML-ENTITIES","auto");
 }
 function toXML($s) {
-    //$s = str_replace('&', '&amp;', $s);
+    $s = str_replace('&', '&amp;', $s);
+    $s = ereg_replace('&amp;&.*;', '&\\1;', $s);
     $s = str_replace('>', '&gt;', $s);
     $s = str_replace('<', '&lt;', $s);
     $s = str_replace('"', '&#34;', $s);
