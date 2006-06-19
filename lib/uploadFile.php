75cfa9cec0b1f64653de2953498cbb17175ebc2c
alexsiri7 <alexsiri7@gmail.com>
2006-06-19 22:06:46 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@421 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ lib/uploadFile.php
@@ -0,0 +1,47 @@
+<?
+require_once $_REQUEST["basedir"]. '/Configuration/pwbapp.php';
+?>
+<html>
+<head>
+</head>
+<body>
+<?
+if (isset($_REQUEST["filenamefield"])){
+?>
+<form id="fm" action="uploadFile.php" method="post" enctype="multipart/form-data">
+<input type="file" id="fileelem"/>
+<input type="hidden" name="nodeid" id="nodeid" value=""/>
+<input type="hidden" name="basedir" id="basedir" value=""/>
+<input type="button"  />
+</form>
+<script type="text/javascript">
+var remf = document.getElementById("fileelem");
+document.getElementById("nodeid").setAttribute('value','<?=$_REQUEST["filenamefield"]?>');
+var doc = window.frameElement.ownerDocument;
+var parwin = doc.window;
+var movf = doc.getElementById('<?=$_REQUEST["filenamefield"]?>');
+document.getElementById("basedir").setAttribute('value', doc.getElementById("basedir").getAttribute('value'));
+parwin.start_uploading('<?=$_REQUEST["filenamefield"]?>');
+remf.parentNode.replaceChild(movf, remf);
+movf.setAttribute('id', 'fileelem');
+movf.setAttribute('name', 'fileelem');
+window.onload=function (){document.getElementById('fm').submit();}
+</script>
+<? } else {
+	$path = split("/", $_REQUEST["nodeid"]);
+	$appclass = $path[1];
+	$app = & Application :: getInstanceOf($appclass);
+	$comp =& $_SESSION['action_dispatcher']->getComponent($_REQUEST["nodeid"], $app);
+	$comp->loadFile($_FILES["fileelem"]);
+?>
+<script>
+var ifr = window.frameElement;
+var doc = ifr.ownerDocument;
+var parwin = doc.window;
+parwin.end_uploading('<?=$_REQUEST["nodeid"]?>', '<?=$_FILES["fileelem"]["name"]?>');
+ifr.parentNode.removeChild(ifr);
+</script>
+<?}?>
+
+</body>
+</html>
