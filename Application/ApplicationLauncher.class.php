5bbe0982b942cec21e53af5c33ce93093b5fe266
alexsiri7 <alexsiri7@gmail.com>
2006-06-20 10:21:55 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@423 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/ApplicationLauncher.class.php
+++ Application/ApplicationLauncher.class.php
@@ -2,8 +2,8 @@
 class ApplicationLauncher {
 	function launch($application_class) {
 		if ($_REQUEST["reset"]=="yes") {
-			session_destroy();
-			session_start();
+			/*session_destroy();
+			session_start();*/
 			unset($_SESSION[sitename][$application_class]);
 		}
 		$app = & Application::getInstanceOf($application_class);
