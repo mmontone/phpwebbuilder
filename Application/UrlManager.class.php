d3102808ac04e90d8a3f60f1b9c250f5fcd18707
alexsiri7 <alexsiri7@gmail.com>
2006-06-26 14:33:25 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@468 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ Application/UrlManager.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class UrlManager extends PWBObject{
+	var $application; 
+    function UrlManager(&$app) {
+    	$this->application =& $app;
+    }
+    function goBack(){
+    	$c =& $this->application->commands->pop();
+    	$c->revert();
+    }
+    function goToUrl($url){
+    	$urls = split('\|', $url);
+    	$bm = $urls[0];
+    	$params=array();
+    	array_shift($urls);
+    	foreach($urls as $u){
+    		$temp = split('=', $u);
+    		$params [$temp[0]] = $temp[1];
+    	}
+    	$this->navigate($bm, $params);
+    }
+    function navigate($bookmark, $params){
+    	$this->application->wholeView->modifications[] = &
+    		new BookmarkXMLNodeModification($this->setBookmarkTarget($bookmark, $params));
+    	$bmc = $bookmark.'Bookmark';
+    	$bm =& new $bmc;
+    	$bm->launchIn($this->application, $params);
+    }
+	function setLinkTarget($bookmark, $params){
+		return 'Action.php?app='.getClass($this->application).'&bookmark='.$this->setBookmarkTarget($bookmark, $params);
+	}
+	function setBookmarkTarget($bookmark, $params=array()){
+		$pss =array($bookmark);
+		foreach($params as $n=>$v) $pss []= $n .'='. $v;
+		$ps = implode('|',$pss);
+		return $ps;
+	}
+}
+?>
