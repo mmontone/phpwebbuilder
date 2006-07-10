fe13a6092563a4ac21a5e698ca824dcca66bdd59
alexsiri7 <alexsiri7@gmail.com>
2006-07-10 18:50:08 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@583 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/ChildCallbackHandler.class.php
+++ Application/ChildCallbackHandler.class.php
@@ -3,10 +3,7 @@
 class ChildCallbackHandler {
     function takeControlOf(&$callbackComponent, $callback=null, $parameters=array()) {
 		if ($callback != null) {
-			if ($callbackComponent->registered_callbacks[$callback] == null) {
-				$this->invalid_callback($callback);
-			}
-			else {
+			if ($callbackComponent->registered_callbacks[$callback] != null) {
 				$callbackComponent->registered_callbacks[$callback]->call($parameters);
 			}
 		}
