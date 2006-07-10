14b85ac8f8134f15fe1c25a1bccd3f501de0931a
alexsiri7 <alexsiri7@gmail.com>
2006-07-10 15:55:40 +0000

    Nicer Logout
    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@576 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/Users/Logout.class.php
+++ DefaultCMS/Users/Logout.class.php
@@ -2,9 +2,9 @@
 
 class Logout extends Component{
 	function initialize(){
-		$this->addComponent(new ActionLink($this, 'logout_do','Logout',$n=null));
+/*		$this->addComponent(new ActionLink($this, 'logout_do','Logout',$n=null));
 	}
-	function logout_do (){
+	function logout_do (){*/
 		User::login('guest','guest');
 		$this->triggerEvent('menuChanged', $n=null);
 		$app =&Application::instance();
