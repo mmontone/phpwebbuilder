a9fd981519614003989140a10535e97f00cea79e
mmontone <marianomontone@gmail.com>
2006-07-21 22:44:58 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@629 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Install/SQLs/minimal-data.php
+++ Install/SQLs/minimal-data.php
@@ -1,22 +1,22 @@
 <?
-$basesql = "INSERT IGNORE INTO  `".baseprefix."MenuSection` VALUES (1, 'Database Structure', 0);
-INSERT IGNORE INTO `".baseprefix."MenuSection` VALUES (2, 'User Admin', 0);
+$basesql = "INSERT IGNORE INTO  `".baseprefix."MenuSection` VALUES (1, 0, 'Database Structure', 0);
+INSERT IGNORE INTO `".baseprefix."MenuSection` VALUES (2, 0, 'User Admin', 0);
 
-INSERT IGNORE INTO `".baseprefix."MenuItem` VALUES (1, 'Database Administration', 'DBController', '', 1);
-INSERT IGNORE INTO `".baseprefix."MenuItem` VALUES (2, 'Roles Administration', 'RolesController', '', 2);
+INSERT IGNORE INTO `".baseprefix."MenuItem` VALUES (1, 0, 'Database Administration', 'DBController', '', 1);
+INSERT IGNORE INTO `".baseprefix."MenuItem` VALUES (2, 0, 'Roles Administration', 'RolesController', '', 2);
 
 
-INSERT IGNORE INTO `".baseprefix."Role` VALUES (1, 'Superuser', '');
-INSERT IGNORE INTO `".baseprefix."Role` VALUES (2, 'Guest', '');
+INSERT IGNORE INTO `".baseprefix."Role` VALUES (1, 0, 'Superuser', '');
+INSERT IGNORE INTO `".baseprefix."Role` VALUES (2, 0, 'Guest', '');
 
-INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (1, 1, 'DatabaseAdmin');
-INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (2, 1, '*');
-INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (3, 1, 'UserAdmin');
-INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (4, 1, 'DatabaseAdmin');
+INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (1, 0, 1, 'DatabaseAdmin');
+INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (2, 0, 1, '*');
+INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (3, 0, 1, 'UserAdmin');
+INSERT IGNORE INTO `".baseprefix."RolePermission` VALUES (4, 0, 1, 'DatabaseAdmin');
 
-INSERT IGNORE INTO `".baseprefix."UserRole` VALUES (1, 1, 1);
-INSERT IGNORE INTO `".baseprefix."UserRole` VALUES (2, 2, 2);
+INSERT IGNORE INTO `".baseprefix."UserRole` VALUES (1, 0, 1, 1);
+INSERT IGNORE INTO `".baseprefix."UserRole` VALUES (2, 0, 2, 2);
 
-INSERT IGNORE INTO `".baseprefix."users` VALUES (1, 'admin', 'PWB-admin');
-INSERT IGNORE INTO `".baseprefix."users` VALUES (2, 'guest', 'guest');"
+INSERT IGNORE INTO `".baseprefix."users` VALUES (1, 0, 'admin', 'PWB-admin');
+INSERT IGNORE INTO `".baseprefix."users` VALUES (2, 0, 'guest', 'guest');"
 ?>
--- Instances/Base/Permissions/User.class.php
+++ Instances/Base/Permissions/User.class.php
@@ -1,77 +1,105 @@
 <?
-
 class User extends PersistentObject {
-	var $permissions = array();
-    function initialize () {
-         $this->table = "users";
-         $this->addField(new textField("user", TRUE));
-         $this->addField(new passwordField("pass", FALSE));
-         $this->addField(new CollectionField("user", array('type'=>'UserRole','display'=>'Roles')));
-    }
-	function &loadUser($user,$pass){
-	$db = DB::Instance();
-	$col = new PersistentCollection('User');
-	$col->conditions["user"] = array("=", "'".$user."'");
-	$col->conditions["pass"] = array("=", "'".$pass."'");
-	$objs = $col->objects();
-	if ($objs===false) {
-		$u =& new User();
-		$u->user->setValue('guest');
-		return $u;
+	var $permissions = array ();
+	function initialize() {
+		$this->table = "users";
+		$this->addField(new textField("user", TRUE));
+		$this->addField(new passwordField("pass", FALSE));
+		$this->addField(new CollectionField("user", array (
+			'type' => 'UserRole',
+			'display' => 'Roles'
+		)));
 	}
-	if (count($objs)>0) {
-		$objs[0]->getPermissions();
-  		return $objs[0];
-	}else
-  		return FALSE;
-    }
-	function getUserId(){
+	function & loadUser($user, $pass) {
+		$db = DB :: Instance();
+		$col = new PersistentCollection('User');
+		$col->conditions["user"] = array (
+			"=",
+			"'" . $user . "'"
+		);
+		$col->conditions["pass"] = array (
+			"=",
+			"'" . $pass . "'"
+		);
+		$objs = $col->objects();
+
+		if (count($objs) > 0) {
+			$objs[0]->getPermissions();
+			return $objs[0];
+		} else
+			return FALSE;
+	}
+	function getUserId() {
 		return $this->getIdOfClass('User');
 	}
-    function hasRole($uid, $roleid){
-    	$urc = new PersistentCollection(UserRole);
-    	$urc->conditions["user"] = array("=", $uid);
-    	$urc->conditions["role"] = array("=", $roleid);
-    	return count($urc->objects())>0;
-    }
-	function &login ($user, $pass) {
-		$usr =& User::loadUser($user, $pass);
-		if ($usr) {
-		    $_SESSION[sitename]["id"] = $usr->getUserId();
-	    	$_SESSION[sitename]["Username"] = $usr->user->getValue();
-			$_SESSION[sitename]["User"] =& $usr;
-  	  	}
-  		return $usr;
+	function hasRole($uid, $roleid) {
+		$urc = new PersistentCollection(UserRole);
+		$urc->conditions["user"] = array (
+			"=",
+			$uid
+		);
+		$urc->conditions["role"] = array (
+			"=",
+			$roleid
+		);
+		return count($urc->objects()) > 0;
 	}
-	function &logged(){
-		if (!isset($_SESSION[sitename]["User"])){
-			User::login('guest','guest');
+	function & login($user, $pass) {
+		$usr = & User :: loadUser($user, $pass);
+
+		if ($usr) {
+			$_SESSION[sitename]["id"] = $usr->getUserId();
+			$_SESSION[sitename]["Username"] = $usr->user->getValue();
+			$_SESSION[sitename]["User"] = & $usr;
 		}
+
+		return $usr;
+	}
+
+	function & logged() {
+		if (!isset ($_SESSION[sitename]["User"])) {
+			$usr =& User :: login('guest', 'guest');
+
+			if (!$usr) {
+				$usr = & new User();
+				$usr->user->setValue('guest');
+				$_SESSION[sitename]["id"] = $usr->getUserId();
+				$_SESSION[sitename]["Username"] = $usr->user->getValue();
+				$_SESSION[sitename]["User"] = & $usr;
+			}
+		}
+
 		return $_SESSION[sitename]["User"];
 	}
-	function getPermissions(){
-        $db =& DB::Instance();
-		$sql = implode(array('SELECT permission FROM ',baseprefix,'UserRole u, ',baseprefix,'RolePermission p',
-				' WHERE user =', $this->getUserId(),
-				' AND u.role=p.role'));
-		 $ps = $db->queryDB($sql);
-		 foreach($ps as $p){
-		 	$this->permissions[]=strtolower($p['permission']);
-		 }
+
+	function getPermissions() {
+		$db = & DB :: Instance();
+		$sql = implode(array (
+			'SELECT permission FROM ',
+			baseprefix,
+			'UserRole u, ',
+			baseprefix,
+			'RolePermission p',
+			' WHERE user =',
+		$this->getUserId(), ' AND u.role=p.role'));
+		$ps = $db->queryDB($sql);
+		foreach ($ps as $p) {
+			$this->permissions[] = strtolower($p['permission']);
+		}
 	}
-	function hasPermission($permission){
+	function hasPermission($permission) {
 		return in_array(strtolower($permission), $this->permissions);
 	}
-	function hasPermissions($permissions){
+	function hasPermissions($permissions) {
 		$ok = false;
-		foreach($permissions as $p){
+		foreach ($permissions as $p) {
 			$ok |= $this->hasPermission($p);
 		}
 		return $ok;
 	}
-	function is_guest(){
-		$u =& User::logged();
-		return $u->user->getValue()=='guest';
+	function is_guest() {
+		$u = & User :: logged();
+		return $u->user->getValue() == 'guest';
 	}
 }
 ?>
