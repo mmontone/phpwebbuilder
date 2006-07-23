b2befe0a789a9ddac46f658f1c18326c6479c420
alexsiri7 <alexsiri7@gmail.com>
2006-07-23 13:42:51 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@642 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/Tests/TestMemoryComponents.class.php
+++ Application/Tests/TestMemoryComponents.class.php
@@ -22,10 +22,18 @@ class TestMemoryComponents extends UnitTestCase {
 		//The memory should be cleaned on a callback
 		$c0 =& $_SESSION['comp'];
 		$c0->addComponent($c1 = new Component);
-		$m = strlen(session_encode());
+		$m = session_encode();
 		$c1->call($c2 = new Component);
 		$c2->callback();
-		$this->assertEqual($m,strlen(session_encode()));
+ 		$regs = array('i:[0-9]*;',
+ 				'\"event_handles\";a:[0-9]*');
+ 		$v2 = session_encode(); $v1 = $m;
+ 		foreach($regs as $reg){
+	 		$v1 = ereg_replace($reg,$reg,$v1);
+	 		$v2 = ereg_replace($reg,$reg,$v2);
+ 		}
+		$this->assertEqual($v1,$v2);
+		$this->assertEqual($lm,strlen(session_encode()));
 	}
 	function testMemory_StopAndCall() {
 		//The memory should be cleaned on a stop and call
