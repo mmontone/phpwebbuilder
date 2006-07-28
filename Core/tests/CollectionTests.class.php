94ed4156c9faf52dd70c123d5db10ab98714de05
mmontone <marianomontone@gmail.com>
2006-07-28 12:51:00 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@679 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Core/tests/CollectionTests.class.php
+++ Core/tests/CollectionTests.class.php
@@ -17,7 +17,26 @@ class CollectionTests extends UnitTestCase {
     		$this->assertEqual($c->at($i), $i);
     	}
 
-    	$this->assertTrue($c->size() == 10);
+    	$this->assertEqual($c->size(), 10);
+    }
+
+    function testPushAndPop() {
+		$c =& new Collection;
+
+    	for ($i = 0; $i < 10; $i++) {
+    		$c->push($i);
+    	}
+
+    	for ($i = 0; $i < 10; $i++) {
+    		$elem =& $c->pop();
+			$this->assertEqual($elem, $i);
+    		/*
+    		$this->assertEqual($c->size(), $i);
+
+    		for ($j = $i - 1; $j >= 0; $j--) {
+    			$this->assertEqual($c->at($j), $j + 1);
+    		}*/
+    	}
     }
 
     function testIsEmpty() {
@@ -34,13 +53,15 @@ class CollectionTests extends UnitTestCase {
 		$this->assertEqual($res, 0);
 
 		$c =& new Collection;
-		$c->add($v = 1);
-		$res = $c->foldr($v = 0,lambda('$acc, $x', 'return $acc + $x;', $a = array()));
+		$c->add($a = 1);
+		$res = $c->foldr($v = 0,lambda('$acc, $x', 'return $acc + $x;', $b = array()));
 		$this->assertEqual($res, 1);
 
 		$c =& new Collection;
+		$temp = array();
 		for ($i = 1; $i <= 5; $i++) {
-    		$c->add($i);
+    		$temp[$i] = $i;
+    		$c->add($temp[$i]);
     	}
 		$res =& $c->foldr($v = 0,lambda('$acc, $x', 'return $acc + $x;', $a = array()));
 		$this->assertEqual($res, 5 + 4 + 3 + 2 + 1);
@@ -51,14 +72,18 @@ class CollectionTests extends UnitTestCase {
 		$c->map(lambda('&$x', 'return $x + 1;', $a = array()));
 		$this->assertTrue($c->isEmpty());
 
+		$temp = array();
 		for ($i = 0; $i < 10; $i++) {
-    		$c->add($i);
+    		$temp[$i] = $i;
+    		$c->add($temp[$i]);
     	}
 
 		$c2 =& $c->map(lambda('$x', 'return $x + 1;', $a = array()));
 
+		$temp = array();
 		for ($i = 0; $i < 10; $i++) {
-    		$this->assertEqual($c2->at($i), $i + 1);
+    		$temp[$i] = $i;
+    		$this->assertEqual($c2->at($i), $temp[$i] + 1);
     	}
     }
 }
