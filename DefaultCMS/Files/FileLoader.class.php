914ac17782ca4cf457906cb4650fa5ff69700450
mmontone <marianomontone@gmail.com>
2006-08-18 21:58:48 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@769 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/Files/FileLoader.class.php
+++ DefaultCMS/Files/FileLoader.class.php
@@ -1,27 +1,98 @@
 <?php
 
 class FileLoader extends Component{
+	var $label_text;
+	var $desc_text;
+
+	function FileLoader() {
+		parent::Component();
+		$this->label_text =& new ValueHolder('');
+		$this->desc_text =& new ValueHolder('');
+	}
+
 	function permissionNeeded () {
 		return "DatabaseAdmin";
 	}
+
     function initialize() {
-		$this->addComponent(new Input(new ValueHolder($lab='')),'label');
-		$this->addComponent(new Filename(new ValueHolder($f='')),'filename');
-		$this->addComponent(new TextAreaComponent(new ValueHolder($des='')),'description');
-		$this->addComponent(new Text(new ValueHolder($t='')),'status');
-		$this->addComponent(new ActionLink($this, 'save', 'Save', $n=null),'save');
-		$this->addComponent(new ActionLink($this, 'callback', 'Cancel', $n=null),'cancel');
+		$this->addLabelInput();
+		$this->addFilenameField();
+		$this->addDescriptionInput();
+		$this->addStatusDisplayer();
+		$this->addUploadButton();
+		$this->addCancelButton();
     }
+
+    function addCancelButton() {
+    	$this->addComponent(new CommandLink(array('text' => Translator::Translate('cancel'), 'proceedFunction' => new FunctionObject($this, 'cancel'))),'cancel');
+    }
+
+    function addUploadButton() {
+    	$this->addComponent(new CommandLink(array('text' => Translator::Translate('save'), 'proceedFunction' => new FunctionObject($this, 'save'))),'save');
+    }
+
+    function addLabelInput() {
+    	$this->addComponent(new Input($this->label_text),'label');
+    }
+
+    function addDescriptionInput() {
+    	$this->addComponent(new TextAreaComponent($this->desc_text),'description');
+    }
+
+    function addStatusDisplayer() {
+    	$this->addComponent(new Text($this->status_text),'status');
+    }
+
     function save(){
-		$file =& $this->filename->file;
-		$file->label->setValue($this->label->getValue());
-		$file->description->setValue($this->description->getValue());
-		$ok = $file->save();
-		if ($ok) {
-			$this->status->setValue($v = 'Upload Successful');
-		} else {
-			$this->status->setValue($v = 'Upload Failed');
+		if (!$this->filename->isFileLoaded()) {
+			$this->uploadFailed();
+			$this->addFilenameField();
+			return false;
+		}
+		else {
+			$file =& $this->filename->file;
+			if ($ex =& $this->checkFile($file)) {
+				$this->uploadFailed($ex->getMessage());
+				$this->addFilenameField();
+				return false;
+			}
+			else {
+				$file->label->setValue($this->label_text->getValue());
+				$file->description->setValue($this->desc_text->getValue());
+
+				if ($this->saveFile(&$file)) {
+					$this->uploadSuccessful();
+				} else {
+					$this->uploadFailed();
+				}
+			}
 		}
     }
+
+    function saveFile(&$file) {
+		return $file->save();
+    }
+
+    function uploadSuccessful() {
+    	$this->status_text->setValue($v = Translator::Translate('Upload Successful'));
+    }
+
+    function uploadFailed($msg = 'Upload failed') {
+    	$this->status_text->setValue($v = Translator::Translate($msg));
+		$this->addFilenameField();
+    }
+
+    function &checkFile(&$file) {
+    	return false;
+    }
+
+    function addFilenameField() {
+    	$this->addComponent(new Filename(new ValueHolder($f='')),'filename');
+    }
+
+    function cancel() {
+		$this->callback('cancel');
+    }
 }
+
 ?>
