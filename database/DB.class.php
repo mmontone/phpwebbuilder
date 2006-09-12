af6655df51dd11a43f9c20067c989d0aea51b51d
alexsiri7 <alexsiri7@gmail.com>
2006-09-12 12:37:38 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@789 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- database/DB.class.php
+++ database/DB.class.php
@@ -16,6 +16,7 @@ class DB {
 			$arr[]="Query suceeded";
 		} else if ($res===false) {
 			$arr[]="Query failed " . mysql_error();
+			trigger_error(E_USER_NOTICE, mysql_error());
 		} else {
 			while ($rec = $this->fetchRecord($res)) $arr[]= $rec;
 		}
