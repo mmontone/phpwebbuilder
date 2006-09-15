04e1a2a30a2ebebcb1bd0f1b3c3d10bfe01797d6
mmontone <marianomontone@gmail.com>
2006-09-15 20:02:58 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@807 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- lib/md.php
+++ lib/md.php
@@ -62,26 +62,75 @@ function _mdcall($function, $arg_types, $i) {
 	}
 }
 
-function &mdcompcall($function, $args) {
+// No se puede usar porque es ineficiente!!! :S
+// No se como hacer multiple dispatch eficiente
+
+function _mdcompcall($function, $count_layers, $arg_types, $i) {
+	$n = count($arg_types);
+	if ($i == $n) {
+		$fname = $function . '_begctx';
+
+
+		for ($j = 0; $j < $count_layers; $j++) {
+			$fname .= '_' . strtoupper($arg_types[$j]);
+		}
+		$fname .= '_endctx';
+
+		for ($j = $count_layers; $j < $n; $j++) {
+			$fname .= '_' . strtoupper($arg_types[$j]);
+		}
+
+		trace('Checking for ' . $fname . '</br>');
+		if (function_exists($fname)) {
+			return $fname;
+		}
+		else {
+			return null;
+		}
+	}
+	else {
+		$fname = _mdcompcall($function, $count_layers, $arg_types, $i + 1);
+		$parent = get_parent_class($arg_types[$i]);
+		while (($fname == null) and $parent) {
+			$arg_types[$i] = $parent;
+			$fname = _mdcompcall($function, $count_layers, $arg_types, $i + 1);
+			$parent = get_parent_class($arg_types[$i]);
+		}
+
+		return $fname;
+	}
+}
+
+
+function &mdcompcall($function, $args, $max_nesting=3) {
 	$c = array();
 	$n = count($args);
 	for ($i = 1; $i < $n ; $i++) {
 		$argi =& $args[$i];
 		if (is_object($argi)) {
-			$c[$i] = get_class($argi);
+			$c[$i-1] = get_class($argi);
 		}
 		else {
-			$c[$i] = gettype($argi);
+			$c[$i-1] = gettype($argi);
 		}
 	}
 
 	$comp =& $args[0];
-	$layers = md_get_layers($comp);
+	$flayers = md_get_layers($comp, $max_nesting);
+	$layers = $flayers;
 	$fname = null;
+
+	$msg = 'Trying to match:<br/>';
+	$msg .= 'Context: ' . print_r($flayers,true) . '<br/>';
+	$msg .= 'Function:' . $function . '(' . print_r($c, true) . ');<br/>';
+	//echo $msg;
 
 	while (!empty($layers) and $fname == null) {
 		$f = $function . '_begctx_' . implode('_', $layers) . '_endctx';
 		$fname = _mdcall($f, $c, 0);
+
+		// _mdcompcall es mas flexible pero extremadamente ineficiente
+		//$fname = _mdcompcall($function, count($layers), array_merge($layers, $c), 0);
 
 		array_shift($layers);
 	}
@@ -97,17 +146,23 @@ function &mdcompcall($function, $args) {
 		return $res;
 	}
 	else {
-		print_backtrace('Dispatch failed: ' . $function . '(' . print_r($c, true) . ');');
+		$msg = 'Dispatch failed: <br/>';
+		$msg .= 'Context: ' . print_r($flayers,true) . '<br/>';
+		$msg .= 'Function:' . $function . '(' . print_r($c, true) . ');<br/>';
+		print_backtrace($msg);
 	}
 }
 
-function md_get_layers(&$comp) {
+function md_get_layers(&$comp, $max_nesting = 3) {
 	$layers = array(strtoupper(get_class($comp)));
 	$c =& $comp->getParent();
 
-	while($c != null) {
+	$i = 0;
+	while(($c !== null) and (getClass($c) != 'stdclass') and ($i < $max_nesting)) {
 		array_push($layers, strtoupper(get_class($c)));
+
 		$c =& $c->getParent();
+		$i++;
 	}
 	$layers = array_reverse($layers);
 	return $layers;
@@ -115,23 +170,34 @@ function md_get_layers(&$comp) {
 
 function load_md_functions($file) {
 	$functions = Spyc::YAMLLoad($file);
-	foreach($functions as $f => $rules) {
-		def_md_function($f, $rules);
+	if (empty($functions)) {
+		print_backtrace('There are no definitions in ' . $file);
+	}
+	else {
+		foreach($functions as $f => $rules) {
+			def_md_function($f, $rules);
+		}
 	}
 }
 
 function def_md_function($f, $rules) {
-	foreach($rules as $ruletype => $rule_def) {
-		eval('$s = def_md_'.$ruletype.'_rule($f, $rule, $rule_def);');
-		//echo 'Defining: ' . $s . '<br/>';
+	foreach($rules as $rule_def) {
+		if (is_array($rule_def['in'])) {
+			$s = def_md_comprule_rule($f, $rule_def);
+		}
+		else {
+			$s = def_md_rule_rule($f, $rule_def);
+		}
+
+		//echo 'Defining: ' . $s . '<br/><br/>';
 		eval($s);
 	}
 }
 
-function def_md_rule_rule($f, $ruletype, $ruledef) {
-	$params = $ruledef['params'];
+function def_md_rule_rule($f, $ruledef) {
+	$params = $ruledef['with'];
 	$body = $ruledef['do'];
-	$s = "function $f";
+	$s = "function &$f";
 
 	if (!is_array($params)) $params = array();
 
@@ -143,9 +209,9 @@ function def_md_rule_rule($f, $ruletype, $ruledef) {
 	return $s;
 }
 
-function def_md_comprule_rule($f, $ruletype, $ruledef) {
-	$context = $ruledef['context'];
-	$params = $ruledef['params'];
+function def_md_comprule_rule($f, $ruledef) {
+	$context = $ruledef['in'];
+	$params = $ruledef['with'];
 	$body = $ruledef['do'];
 	$s = "function $f";
 	$s .= '_begctx';
@@ -164,5 +230,6 @@ function def_md_comprule_rule($f, $ruletype, $ruledef) {
 	$s .= '(' . implode(',', $ps) . ') {' . $body . '}';
 	return $s;
 }
+
 
 ?>
