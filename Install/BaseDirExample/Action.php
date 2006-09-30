aa8f44dcbfedd4baa333b0b76ce8858a46d66293
mmontone <marianomontone@gmail.com>
2006-09-30 22:25:29 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@840 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Install/BaseDirExample/Action.php
+++ Install/BaseDirExample/Action.php
@@ -1,7 +1,12 @@
 <?php
-ini_set('memory_limit', '32M');
+
 if(isset($_REQUEST["app"])) define('app_class',$_REQUEST["app"]);
 require_once dirname(__FILE__).'/Configuration/pwbapp.php';
+
+ob_start('fatal_error_handler');
+
 Application::launch();
+
+ob_end_flush();
 
 ?>
