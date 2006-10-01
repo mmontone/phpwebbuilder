04e48bbc97c261eff9636e6f5b45cd8d908954b0
mmontone <marianomontone@gmail.com>
2006-10-01 18:14:06 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@845 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/components/QuestionDialog.class.php
+++ Application/components/QuestionDialog.class.php
@@ -2,16 +2,15 @@
 
 class QuestionDialog extends Component
 {
-	var $question;
+	var $message;
 
-	function QuestionDialog($question, $callback_actions=array('on_yes'=>'question_accepted', 'on_no' => 'question_cancelled')) {
-		$this->question = $question;
+	function QuestionDialog($question) {
+		$this->message = $question;
 		parent::Component();
-		$this->registerCallbacks($callback_actions);
 	}
 
 	function initialize(){
-		$this->addComponent(new Label($this->question), 'question');
+		$this->addComponent(new Label($this->message), 'question');
 		$this->addComponent(new CommandLink(array('proceedFunction' => new FunctionObject($this, 'yes'),
 		                                          'text' => 'Yes'),'yes'), 'yes');
 		$this->addComponent(new CommandLink(array('proceedFunction' => new FunctionObject($this, 'no'), 'text' => 'No'), 'no'),'no');
