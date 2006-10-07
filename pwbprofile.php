987919abddb3d0f2adb3b16fead331756248ee01
alexsiri7 <alexsiri7@gmail.com>
2006-10-07 12:45:36 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@865 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- pwbprofile.php
+++ pwbprofile.php
@@ -1,8 +1,15 @@
 <?
 
 xdebug_start_profiling();
+
+function showProfile(){
+	xdebug_dump_function_profile(4);
+}
+
+register_shutdown_function('showProfile');
+
 require_once 'pwb.php';
-xdebug_dump_function_profile(4);
+
 
 
 ?>
