ff0a7704548f1157fd58d5f163d346ddb0d15e00
mmontone <marianomontone@gmail.com>
2006-10-09 15:56:36 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@881 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- View/Handler/HTMLHandler/FilenameHTMLHandler.class.php
+++ View/Handler/HTMLHandler/FilenameHTMLHandler.class.php
@@ -3,7 +3,10 @@
 class FilenameHTMLHandler extends InputHTMLHandler{
 	function initializeDefaultView(&$view) {
 		$view->setTagName('iframe');
+		$view->appendChild(new XMLTextNode('&nbsp;'));
+
 		$view->setAttribute('src', toAjax(pwb_url.'/lib/uploadFile.php' .
+
 								'?basedir='.basedir.'&filenamefield='.$this->component->getId()
 								. '&app='.app_class));
 		$view->addCSSClass('uploadfile');
