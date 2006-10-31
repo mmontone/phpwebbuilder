05b7a6f8d491b65aebe97be779f8cdd44e339ab0
alexsiri7 <alexsiri7@gmail.com>
2006-10-31 23:01:18 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@942 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/Component.class.php
+++ Application/Component.class.php
@@ -159,6 +159,7 @@ class Component extends PWBObject
 		}
 	}
 	function delete(){
+		$this->view->parent->removeChild($this->view);
 		$h =& $this->holder;
 		$p =& $h->parent;
 		$pos =&  $h->__owner_index;
