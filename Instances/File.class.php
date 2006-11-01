23633a2c33c786939d38a2201d2cafddd6fe60c3
alexsiri7 <alexsiri7@gmail.com>
2006-11-01 14:33:38 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@947 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Instances/File.class.php
+++ Instances/File.class.php
@@ -3,10 +3,10 @@
 class File extends PersistentObject {
       function initialize () {
                $this->table = "files";
-               $this->addField(new TextField('label', true));
-               $this->addField(new FilenameField("filename", true));
+               $this->addField(new TextField('label', FALSE));
+               $this->addField(new FilenameField("filename", FALSE));
                $this->addField(new NumField("filesize", false));
-               $this->addField(new textField("filetype", true));
+               $this->addField(new textField("filetype", FALSE));
                $this->addField(new BlobField("bin_data", FALSE));
                $this->addField(new TextArea("description", FALSE));
        }
