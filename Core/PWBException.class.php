e6377591d6c227553c01a693b1c0d218c9c04a1a
mmontone <marianomontone@gmail.com>
2006-11-03 14:12:19 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@962 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ Core/PWBException.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class PWBException extends PWBObject {
+	var $message;
+	var $content;
+	var $backtrace;
+
+    function createInstance($params) {
+		$this->message =& $params['message'];
+    	$this->content =& $params['content'];
+    	$this->backtrace = debug_backtrace();
+	}
+
+    function raise(&$handler) {
+    	$handler->callWith($this);
+    }
+
+    function getMessage() {
+		return $this->message;
+    }
+
+    function &getContent() {
+    	return $this->content;
+    }
+
+    function isException() {
+    	return true;
+    }
+
+    function accept(&$visitor) {
+    	return $visitor->visitPWBException($this);
+    }
+}
+
+?>
