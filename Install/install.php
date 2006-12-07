67f3998ac4194d5be58c1e6054eee4bc28109a11
mmontone <marianomontone@gmail.com>
2006-12-07 14:12:10 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1056 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Install/install.php
+++ Install/install.php
@@ -3,8 +3,8 @@
 define('modules', 'Core,Application,View,database,DefaultCMS/Administrator,Model,BugNotifier,QuicKlick');
 define('app_class', 'InstallApplication');
 define('pwbdir', dirname(__FILE__).'/../');
-define('basedir', dirname(__FILE__));
-define('app', 'InstallInstances');
+define('basedir', '');
+define('app', 'InstallInstances/');
 define('page_renderer', 'StandardPageRenderer');
 define('debugview', 'false');
 define('templates', 'enabled');
