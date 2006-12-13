f9e10a69c3294ec90d83a6037defba59bb6887e3
alexsiri7 <alexsiri7@gmail.com>
2006-12-13 18:54:10 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1069 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Install/install.php
+++ Install/install.php
@@ -2,15 +2,18 @@
 
 define('modules', 'Core,Application,View,database,DefaultCMS/Administrator,Model,BugNotifier,QuicKlick');
 define('app_class', 'InstallApplication');
-define('pwbdir', dirname(__FILE__).'/../');
+define('pwbdir', dirname(dirname(__FILE__)).'/');
 define('basedir', '');
-define('app', 'InstallInstances/');
+define('app', 'InstallInstances');
+
 define('page_renderer', 'StandardPageRenderer');
 define('debugview', 'false');
 define('templates', 'enabled');
 define('error_reporting', E_ERROR | E_WARNING);
 define('site_url', '');
 define('pwb_url', '../');
+define('compile', '');
 session_name(strtolower(app_class));
+require_once dirname(__FILE__).'/BaseDirExample/Configuration/ConfigReader.class.php';
 require_once dirname(__FILE__).'/../pwb.php';
 ?>
