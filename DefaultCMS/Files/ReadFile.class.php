4722fe582ca9f97834472869216bd5207f43d48e
alexsiri7 <alexsiri7@gmail.com>
2006-12-18 17:09:49 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1090 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- DefaultCMS/Files/ReadFile.class.php
+++ DefaultCMS/Files/ReadFile.class.php
@@ -1,11 +1,18 @@
 <?php
 
+
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')){
+	session_cache_limiter("public");
+}
+
+
 class ReadFile {
 	function render(){
 		$obj =& File::getWithId('File', $_REQUEST["fileid"]);
 		header('Content-Type: '.$obj->filetype->getValue());
+		header('Content-Length: '.$obj->filesize->getValue());
 		header('Content-Disposition: attachment; filename="'.$obj->filename->getValue().'"');
-		print $obj->bin_data->getValue();
+		print $obj->bin_data->getCompleteValue();
 	}
 }
 ?>
