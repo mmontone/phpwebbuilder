8f5f6086403700b9adcf4cbce565702a6a5fb4df
alexsiri7 <alexsiri7@gmail.com>
2007-01-16 19:53:51 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1152 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- View/PageRenderer.class.php
+++ View/PageRenderer.class.php
@@ -280,7 +280,7 @@ class AjaxPageRenderer extends PageRenderer {
 		header("Content-type: text/xml");
 		$xml = '<?xml version="1.0" encoding="ISO-8859-1" ?>';
 		$xml .= "\n<ajax>";
-		$xml .= $view->renderAjaxResponseCommand();
+		$xml .= $win->wholeView->renderAjaxResponseCommand();
 		$xml .= $this->renderJSCommands($win);
 		$xml .= "</ajax>";
 
