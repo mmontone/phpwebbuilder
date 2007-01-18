69c8f5b22e4ca15c2cd9affe4b78f32e98e09a96
alexsiri7 <alexsiri7@gmail.com>
2007-01-18 12:09:45 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1158 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/components/Widgets/HTML.class.php
+++ Application/components/Widgets/HTML.class.php
@@ -1,9 +1,15 @@
 <?php
 
 class HTML extends Widget {
-	function HTML($string){
-		#@typecheck $string:string#@
-		parent::Widget(new ValueHolder($string));
+	function HTML(&$string){
+
+		if (is_object($string)) {
+			#@typecheck $string:ValueModel#@
+			parent::Widget($string);
+		} else {
+			#@typecheck $string:string#@
+			parent::Widget(new ValueHolder($string));
+		}
 	}
 	function setEvents() {}
 }
