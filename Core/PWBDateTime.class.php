e72b418d1b27fcb7a1c0fc764b41c5f07b40d88c
alexsiri7 <alexsiri7@gmail.com>
2007-02-28 20:30:07 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1303 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ Core/PWBDateTime.class.php
@@ -0,0 +1,61 @@
+<?php
+
+class PWBDateTime extends PWBObject{
+    var $date;
+
+    function PWBDateTime($date='') {
+    	$this->setValue($date);
+    	parent::PWBObject();
+    }
+    function setValue($date){
+    	#@check is_string($date)@#
+    	$this->date = $date;
+    	$this->triggerEvent('changed', $date);
+    }
+    function getValue(){return$this->date;}
+    function &dateArray () {
+        return getDate(strtotime($this->date));
+    }
+    function dateDiff(&$date){
+        #@typecheck $date: PWBDateTime@#
+        $d1 =& $this->dateArray();
+        $d2 =& $date->dateArray();
+        foreach ($d1 as $ind=>$val) {
+            $d3[$ind] = $val - $d2[$ind];
+        }
+        return $d3;
+    }
+    function printString(){
+    	return $this->date;
+    }
+    function validateDate() {
+    	return ereg('([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})',$this->date, $matches) && checkdate($matches[2], $matches[3], $matches[1]);
+    	//return ereg('([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})',$this->date, $matches);
+    }
+    function before($date){
+    	#@typecheck $date: PWBDateTime@#
+    	return strtotime($this->date) < strtotime($date->date);
+    }
+
+    function now(){
+        return new PWBDateTime(PWBDateTime::format(getDate()));
+    }
+    function format ($date) {
+        return PWBDateTime::dateFormat($date)." ".PWBDateTime::timeFormat($date);
+    }
+    function setDateArr(&$arr){
+        $this->setValue($this->format($arr));
+    }
+    function validateTime() {
+    	//return ereg('([0-9]{2})((:([0-9]{1,2})){0,2})', $this->date, $matches) && $matches[0] <'24' && $matches[1] <'60' && $matches[2] <'60';
+    	return ereg('([0-9]{2})((:([0-9]{1,2})){0,2})', $this->date, $matches);
+    }
+    function timeformat ($date) {
+        return $date["hours"].":".$date["minutes"].":".$date["seconds"];
+    }
+
+    function dateformat ($date) {
+        return $date["year"]."-".$date["mon"]."-".$date["mday"];
+    }
+}
+?>
