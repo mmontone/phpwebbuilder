cb293dbd401c25349b4afe0ac1c905dc785cb5b2
alexsiri7 <alexsiri7@gmail.com>
2007-04-29 13:34:33 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1498 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- doc/paper/compile.sh
+++ doc/paper/compile.sh
@@ -2,6 +2,8 @@
 for i in `ls src/*.php`; do
   cat $i | grep -v "<?php" | grep -v "?>" | lgrind -lphp -i - > $i.tex;
 done
+cat src/consulta.sql | lgrind -lsql -i - > src/consulta.sql.tex;
+
 pdflatex presentation;
 bibtex presentation;
 pdflatex presentation;
