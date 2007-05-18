cc1212eb8523079a1c556b101f8666f2ba707ca6
alexsiri7 <alexsiri7@gmail.com>
2007-05-18 14:34:54 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1560 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- YATTAA/YATTAA.php
+++ YATTAA/YATTAA.php
@@ -2,6 +2,7 @@
 
 compile_once (dirname(__FILE__).'/ContextualComponent.class.php');
 compile_once (dirname(__FILE__).'/ModuleComponent.class.php');
+compile_once (dirname(__FILE__).'/ObjectAdmin.class.php');
 compile_once (dirname(__FILE__).'/ContextMenus.class.php');
 compile_once (dirname(__FILE__).'/ObjectsAdmin.class.php');
 compile_once (dirname(__FILE__).'/CollectionNavigator.class.php');
