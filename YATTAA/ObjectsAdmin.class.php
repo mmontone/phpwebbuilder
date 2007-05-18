ed5dcc132fbd04b742e30cb01d165e86e7873259
alexsiri7 <alexsiri7@gmail.com>
2007-05-18 14:12:56 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1558 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

---
+++ YATTAA/ObjectsAdmin.class.php
@@ -0,0 +1,86 @@
+<?php
+
+class ObjectsAdmin extends ContextualComponent {
+	var $objects;
+	function ObjectsAdmin(&$objects) {
+		$this->objects =& $objects;
+
+		parent::ContextualComponent();
+	}
+	function getTitle(){
+		return $this->listObjectsMessage();
+	}
+    function setTitle($title) {
+        $this->addComponent(new Label($title), 'admin_title');
+    }
+
+	function initialize() {
+		$this->displayList();
+		$this->addActionMenu($this->newObjectMessage(), new FunctionObject($this, 'newObject'));
+	}
+
+	function &getObjects() {
+		return $this->objects;
+	}
+
+	function changeBody(&$body) {
+		$this->addComponent($body, 'body');
+	}
+
+	function &displayList() {
+		$list =& $this->listComponentFor($this->getObjects());
+		$list->registerCallback('element_selected', new FunctionObject($this, 'elementSelected'));
+		$this->changeBody($list);
+		return $list;
+	}
+
+	function elementSelected(&$object) {
+		$this->adminObject($object);
+	}
+
+	function refreshCollection() {
+		$this->body->refresh();
+	}
+
+	function newObject() {
+		$creator =& $this->getCreatorComponent();
+		$this->call($creator);
+	}
+
+	function objectCreated(&$obj) {
+		$this->adminObject($obj);
+	}
+
+	function &adminObject(&$obj) {
+		$admin =& $this->getAdminComponent($obj);
+        $this->call($admin);
+		return $admin;
+	}
+
+	function &getAdminComponent(&$dt) {
+		$admin =& $this->adminComponentFor($dt);
+		return $admin;
+	}
+
+	function newObjectMessage() {
+		return Translator::Translate('New ' . strtolower($this->objects->dataType));
+	}
+
+	function listObjectsMessage() {
+		return Translator::Translate(ucfirst($this->objects->dataType) . 's');
+	}
+
+	function &adminComponentFor(&$object) {
+		return mdcall('getAdminComponent', array(&$this, &$object));
+	}
+
+	function &getCreatorComponent() {
+		return mdcompcall('getObjectCreator', array(&$this));
+	}
+
+	function &listComponentFor(&$objects) {
+		return mdcompcall('getListComponent',array(&$this, &$objects));
+	}
+}
+
+?>
---
+++ YATTAA/YATTAA.php
@@ -0,0 +1,10 @@
+<?php
+
+compile_once (dirname(__FILE__).'/ContextualComponent.class.php');
+compile_once (dirname(__FILE__).'/ModuleComponent.class.php');
+compile_once (dirname(__FILE__).'/ContextMenus.class.php');
+compile_once (dirname(__FILE__).'/ObjectsAdmin.class.php');
+compile_once (dirname(__FILE__).'/CollectionNavigator.class.php');
+
+
+?>
