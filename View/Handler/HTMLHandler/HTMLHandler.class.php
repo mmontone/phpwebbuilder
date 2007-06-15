1f3a85b8fd0a20bc9acf833c827f22f01c983315
alexsiri7 <alexsiri7@gmail.com>
2007-06-15 13:39:02 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1637 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- View/Handler/HTMLHandler/HTMLHandler.class.php
+++ View/Handler/HTMLHandler/HTMLHandler.class.php
@@ -13,13 +13,12 @@ class ComponentHTMLHandler extends HTMLHandler{
 		parent::setComponent($comp);
 		$this->setEvents($comp);
 	}
-	function setEvents(&$comp){
-		$comp->componentstates->onChangeSend('updateStates',$this);
-	}
+	function setEvents(&$comp){}
 	function setView(& $view) {
 		parent :: setView($view);
 		$this->component->componentstates->addAll($view->css_classes);
 		$this->updateStates($this->component->componentstates, $n=null);
+		$this->component->componentstates->onChangeSend('updateStates',$this);
 	}
 	function updateState(&$col, $ev){
 		$this->view->addCSSClass($ev);
