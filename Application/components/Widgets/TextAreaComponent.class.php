014ed32161fc9fc469eed32ff11d3bd36b375471
alexsiri7 <alexsiri7@gmail.com>
2007-10-19 19:02:14 +0000

    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1714 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Application/components/Widgets/TextAreaComponent.class.php
+++ Application/components/Widgets/TextAreaComponent.class.php
@@ -4,6 +4,11 @@ class TextAreaComponent extends Widget{
 	function printValue(){
 		return $this->value_model->getValue();
 	}
+	function viewUpdated($params) {
+		//&#38;Acirc bug in IE
+		if (ord(substr($params,0,1))==194){$params = substr($params,1);}
+		parent::viewUpdated($params);
+	}
 }
 
 ?>
