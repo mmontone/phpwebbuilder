6d5151c25a00cdfc572d66fd0296fe65ea96524d
alexsiri7 <alexsiri7@gmail.com>
2009-10-12 21:05:17 +0000

    non numeric value allowed for num field
    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1878 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Model/Fields/NumField.class.php
+++ Model/Fields/NumField.class.php
@@ -5,8 +5,6 @@ class NumField extends DataField {
 	function createInstance($params){
 		parent::createInstance($params);
 		$this->range = @$params['range'];
-		$this->value=0;
-		$this->buffered_value=0;
 	}
 	function increment(){
 		$this->setValue($this->getValue()+1);
@@ -15,7 +13,7 @@ class NumField extends DataField {
 		$this->setValue($this->getValue()-1);
 	}
 	function defaultValues($params){
-		return array_merge(array('range'=>null, 'numtype'=>'int'), parent::defaultValues($params));
+		return array_merge(array('default'=>0, 'range'=>null, 'numtype'=>'int'), parent::defaultValues($params));
 	}
 	function isRanged() {
 		return $this->range != null;
