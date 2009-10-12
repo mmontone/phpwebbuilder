bb74ca9528a643e8f9cbfc25f0c2bf33a623f9d4
alexsiri7 <alexsiri7@gmail.com>
2009-10-12 21:04:12 +0000

    notice
    git-svn-id: https://phpwebbuilder.svn.sourceforge.net/svnroot/phpwebbuilder/trunk@1877 e3e4e2bd-1310-0410-aae6-9fb0b3a765dc

--- Model/Fields/EmailField.class.php
+++ Model/Fields/EmailField.class.php
@@ -8,18 +8,22 @@ class EmailField extends TextField {
 	function SQLvalue() {
 		return "'" . $this->getValue() . "'" . ", ";
 	}
-	function &validate() {
-		if ($this->getValue()=='') return false;
-		$atom = '[-A-Za-z0-9!#$%&\'*+/=?^_`{|}~]'; // allowed characters for part before "at" character
-		$domain = '([A-Za-z]([-A-Za-z0-9]*[A-Za-z0-9]+)?)'; // allowed characters for part after "at" character
-			$regex = '^' . $atom . '+' . // One or more atom characters.
+	function & validate() {
+		if ($this->getValue() == '') {
+			$val = false;
+			return $val;
+		} else {
+			$atom = '[-A-Za-z0-9!#$%&\'*+/=?^_`{|}~]'; // allowed characters for part before "at" character
+			$domain = '([A-Za-z]([-A-Za-z0-9]*[A-Za-z0-9]+)?)'; // allowed characters for part after "at" character
+				$regex = '^' . $atom . '+' . // One or more atom characters.
 		'(\.' . $atom . '+)*' . // Followed by zero or more dot separated sets of one or more atom characters.
 		'@' . // Followed by an "at" character.
 		'(' . $domain . '{1,63}\.)+' . // Followed by one or max 63 domain characters (dot separated).
 		$domain . '{2,63}' . // Must be followed by one set consisting a period of two
-		'$'; // or max 63 domain characters.
-		return $this->validate_ereg($regex,$this->displayString . ' '.Translator::translate('is not a valid email'));
-
+	'$'; // or max 63 domain characters.
+			$val = & $this->validate_ereg($regex, $this->displayString . ' ' . Translator :: translate('is not a valid email'));
+			return $val;
+		}
 	}
 }
 ?>
